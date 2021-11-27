318e435ba63492aabc68480312c55273ae09ba7b
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-11-27 16:10:03 -0500

    timer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react"
-
-
+import React, { useState, useEffect } from "react"
 
 function App() {
   const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(5)
+  const [gameStatus, setGameStatus] = useState(false)
 
   function handleChange(e) {
     const { value } = e.target
@@ -15,6 +15,19 @@ function App() {
     return wordsArr.filter(word => word !== "").length
   }
 
+  function handleGameStatus(gameStatus){
+    setGameStatus(prevStatus => (!prevStatus))
+  }
+
+  useEffect(() => {
+    if (timeRemaining > 0 && gameStatus) {
+      setTimeout(() => {
+        setTimeRemaining(time => time - 1)
+      }, 1000)
+    }
+  }, [timeRemaining, gameStatus])
+
+
   return (
     <div>
       <h1>How fast do you type?</h1>
@@ -22,9 +35,9 @@ function App() {
         onChange={handleChange}
         value={text}
       />
-      <h4>Time remaining: ???</h4>
-      <button onClick={() => console.log(calculateWordCount(text))}>Start</button>
-      <h1>Word count: ???</h1>
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={() => handleGameStatus(gameStatus)}>Start</button>
+      <h1 onChange={calculateWordCount(text)}>Word count: ???</h1>
     </div>
   )
 }

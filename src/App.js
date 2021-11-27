7a5edd24912155f8c6f54078aa47379cc6441948
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-11-27 13:45:33 -0500

    font + styles added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
 
 function App() {
   return (
     <div>
+      <h1>Speed Typers</h1>
+      <textarea
+        value={""}
+      ></textarea>
+      <h4>Time Remaining: </h4>
+      <h1>Word Count:</h1>
     </div>
   );
 }

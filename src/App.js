0bf548015e1b99a184b24ce75e0ae16319809d27
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-11-27 14:08:25 -0500

    state created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 
+import React, { useState, useEffect } from 'react';
+
 function App() {
+  const [text, setText] = useState("");
+
+  const handleTyping = (event) => {
+    const { value } = event.target.value
+    setText(value)
+  }
+
+  console.log(text);
+
   return (
     <div>
       <h1>Speed Typers</h1>
       <textarea
-        value={""}
+        value={text}
+        name="textArea"
+        onChange={handleTyping}
       ></textarea>
       <h4>Time Remaining: </h4>
-      <h1>Word Count:</h1>
+      <h1>Word Count: {text.length}</h1>
     </div>
   );
 }
